fe6173c330feb758617f873bf37fa71bca6e8060
Demon Dev <demonic-dev@protonmail.com>
2022-04-05 06:24:49 +0430

    check game play and chosen cards order

--- src/project/utils/tests/test_game_play.py
+++ src/project/utils/tests/test_game_play.py
@@ -1,4 +1,3 @@
-import json
 from random import choice
 from django.test import TestCase
 from project.round.models import Round
@@ -6,13 +5,14 @@ from project.nft.models import NFT, FullDeck, ChosenCard
 from project.utils.collectigame_algorithm.src.matrix_utils.generate_random_nfts import GenerateRandomNFT
 from project.utils.collectigame_algorithm.src.matrix_utils.shuffle import Suffle
 from project.utils.collectigame_algorithm.src.matrix_utils.play_lottery import PlayLottery
-from project.nft.enums import BloodType, Rarity, CounterType, Side
-
-from project.nft.api.serializers import FullDeckSerializer
+from project.nft.enums import BloodType, Rarity, CounterType
 
 
 class GameTestCase(TestCase):
     def setUp(self):
+        self.generate_random_nfts()
+
+    def generate_random_nfts(self):
         nft = []
         nft_obj = GenerateRandomNFT()
         r_nfts = nft_obj.generate_random_nfts()
@@ -47,36 +47,32 @@ class GameTestCase(TestCase):
 
             )
         NFT.objects.bulk_create(nft)
-        eth_block = "0xe890dbf5ee13f4f43d6777b5afcb5e8913af15ea3d8eb9dc6cb03852df8b147c"
-        self.round = Round.objects.create_round(66.66, eth_block)
+        self.eth_block = "0xe890dbf5ee13f4f43d6777b5afcb5e8913af15ea3d8eb9dc6cb03852df8b147c"
+        self.round = Round.objects.create_round(66.66, self.eth_block)
 
-        shuffle_obj = Suffle(5000)
-        shuffled_obj = shuffle_obj.randomize(eth_block)
+        self.shuffle_obj = Suffle(5000)
+        shuffled_obj = self.shuffle_obj.randomize(self.eth_block)
         deck = []
         starter, sorted_dict = shuffled_obj
-        print(starter)
+        # print(starter)
         for i,(k,v) in enumerate(sorted_dict.items()):
             deck.append(FullDeck(
-                nft_id=v,
+                nft_id=i+1,
                 round=self.round,
-                deck_place=i+1,
+                deck_place=v,
                 wallet="0xwallet" + str(i+1),
                 is_starter=True if v == starter else False
             ))
         FullDeck.objects.bulk_create(deck)
         self.starter = FullDeck.objects.get(is_starter=True, round=self.round)
         self.starter_deck_place = self.starter.deck_place
-        print(self.starter)
 
-    def test_can_play_game(self):
-        # full_deck = json.dumps(FullDeckSerializer(FullDeck.objects.filter(round=self.round), many=True).data)
-        # json_data = json.loads(full_deck)
-        # for item in json_data:
-        #     print("GG ", item)
         full_deck = FullDeck.objects.filter(round=self.round).select_related("nft")
         lottery = PlayLottery(full_deck, self.starter_deck_place)
-        winners, chosen_cards = lottery.play()
-        ChosenCardList = [ChosenCard(**card) for card in chosen_cards]
-        ChosenCard.objects.bulk_create(ChosenCardList)
-        print("winner:", winners, "chosen cards:", chosen_cards)
-        self.assertEqual(1, 1)
+        self.winners, self.chosen_cards = lottery.play()
+        self.chosen_card_list = [ChosenCard(**card, round=self.round) for card in self.chosen_cards]
+        ChosenCard.objects.bulk_create(self.chosen_card_list)
+        # print(self.starter)
+
+    def test_can_play_game(self):
+        self.assertGreater(len(self.winners), 0)
