8e94cb76020b0c3fef4947437021a1a0ff5f5c2a
Demon Dev <demonic-dev@protonmail.com>
2022-04-05 06:22:28 +0430

    round field in chosen_card model

--- src/project/nft/models/chosen_card.py
+++ src/project/nft/models/chosen_card.py
@@ -12,11 +12,22 @@ class ChosenCard(BaseModel):
         null=True,
         blank=True
     )
+    round = models.ForeignKey(
+        verbose_name=_("Round"),
+        to="round.Round",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
     total_counter = models.PositiveIntegerField(
         verbose_name=_("Total counter"),
         null=True
     )
     magnet_status = models.BooleanField(
+        verbose_name=_("Magnet status"),
+        default=False
+    )
+    magnet_stole = models.BooleanField(
         verbose_name=_("Magnet status"),
         default=False
     )
