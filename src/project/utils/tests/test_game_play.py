659ef6f2cf84e0fcb3536d14b2dafdbf6fc57269
Demon Dev <demonic-dev@protonmail.com>
2022-04-05 06:26:43 +0430

    check if prev nft to latest one is thief

--- src/project/utils/tests/test_game_play.py
+++ src/project/utils/tests/test_game_play.py
@@ -145,3 +145,25 @@ class GameTestCase(TestCase):
         lottery = PlayLottery(full_deck, self.starter_deck_place)
         winners, chosen_cards = lottery.play()
         self.assertEqual(winners[0].deck_place, next_to_last_id)
+
+    def test_prev_is_thief(self):
+        last = ChosenCard.objects.filter(round=self.round).order_by("id").last()
+        last.speciality=CounterType.NOTHING
+        last.save()
+        before_last =  ChosenCard.objects.get(id = last.id - 1)
+        next_to_last_id = 1 if last.deck_place.deck_place == NFT.objects.count() else last.deck_place.deck_place + 1
+        prev_to_last_id = NFT.objects.count() if last.deck_place.deck_place == 1 else last.deck_place.deck_place - 1
+
+        # turns magnet on
+        NFT.objects.filter(id=FullDeck.objects.get(deck_place=before_last.deck_place.deck_place).nft.id).update(
+            speciality=CounterType.MAGNET)
+
+        NFT.objects.filter(id=FullDeck.objects.get(deck_place=next_to_last_id).nft.id).update(
+            speciality=CounterType.NOTHING)
+        NFT.objects.filter(id=FullDeck.objects.get(deck_place=prev_to_last_id).nft.id).update(
+            speciality=CounterType.MAGNET)
+
+        full_deck = FullDeck.objects.filter(round=self.round).select_related("nft")
+        lottery = PlayLottery(full_deck, self.starter_deck_place)
+        winners, chosen_cards = lottery.play()
+        self.assertEqual(winners[0].deck_place, prev_to_last_id)
