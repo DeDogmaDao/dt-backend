c9dc651e79b8ae61bd6ef70c31cc7d10590b1619
Demon Dev <demonic-dev@protonmail.com>
2022-05-20 04:18:51 +0430

    changed to 10 secs

--- src/project/contract_history/tasks.py
+++ src/project/contract_history/tasks.py
@@ -1,8 +1,6 @@
-from celery.schedules import crontab
 from celery.utils.log import get_task_logger
 
 from project import celery_app
-from django.utils import timezone
 from project.utils.base_task import BaseTask
 
 from project.utils.blockchain_event_scanner import run_fetch
@@ -13,13 +11,9 @@ class FetchContractHistoryTask(BaseTask):
     logger = get_task_logger(__name__)
 
     def _run(self, *args, **kwargs):
-        data = self.fetch_data()
-        run_fetch("https://mainnet.infura.io/v3/9317e901ece9489c8fa9b44078283316")
-        print(data, timezone.now())
-
-    def fetch_data(self):
-        return "stupid data"
+        run_fetch(
+            "https://mainnet.infura.io/v3/9317e901ece9489c8fa9b44078283316")  # TODO: add infura url into github secrets
 
 
 celery_app.tasks.register(FetchContractHistoryTask())
-celery_app.add_periodic_task(5.0, FetchContractHistoryTask())
+celery_app.add_periodic_task(10.0, FetchContractHistoryTask())
